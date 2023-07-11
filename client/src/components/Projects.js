2c94c1d03cc9f37f16f82a1b0035d1e9bc877bc8
Paul Henstridge <paulhenstridge@gmail.com>
2023-07-11 15:54:46 +0100

    added scroll links

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -2,8 +2,12 @@ import styled from "styled-components";
 import PageScroll from "./PageScroll";
 
 const ProjectsContainer = styled.section`
-    height: 100vh;
+  height: 100vh;
     background-color: #ad5ad3;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
 `
 
 const Projects = () => {
