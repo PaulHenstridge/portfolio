a36bca5453e94a34c169e3f4df1616ab52399625
Paul Henstridge <paulhenstridge@gmail.com>
2023-07-21 15:18:40 +0100

    working on experience section layout

--- client/src/components/ExpPanel.js
+++ client/src/components/ExpPanel.js
@@ -2,26 +2,18 @@ import React, { useState } from "react"
 
 import styled from "styled-components"
 
-const ExpPanel = ({ data, bg }) => {
+const ExpPanel = ({ data, bg, active, isPrimary, onClick }) => {
 
-    const [isClicked, setIsClicked] = useState(false);
 
-    const handleClick = () => {
-        setIsClicked(!isClicked);
-    }
     const ExpContainer = styled.article`
-        position:absolute;
         border: 8px solid rgb(13, 20, 34);
-        width:${isClicked ? '100%' : props => props.data.styles.width};
-        height:${isClicked ? '100%' : props => props.data.styles.height};
-        top: ${isClicked ? '0' : props => props.data.styles.top};
-        left: ${isClicked ? '0' : props => props.data.styles.left};
-        z-index: ${isClicked ? 2 : 0};
+        z-index: ${({ active }) => active ? 2 : 1};
         transition: all 0.5s ease;
         background-image:url(${props => props.bg});
         background-size:cover;
         background-position:right;
-
+        /* grid-area: ${({ active, isPrimary }) => active ? '1 / 1 / span 2 / span 2' : (isPrimary ? '1 / 1 / span 2 / auto' : 'auto')}; */
+        /* visibility: ${({ active }) => active ? 'visible' : 'hidden'}; */
     `
 
     let bullets = data.bullets.map((bullet, index) => {
@@ -29,7 +21,7 @@ const ExpPanel = ({ data, bg }) => {
     })
 
     return (
-        <ExpContainer onClick={handleClick} data={data} bg={bg}>
+        <ExpContainer onClick={onClick} data={data} bg={bg} active={active} isPrimary={isPrimary}>
             {data.values}
             <h4>{data.header}</h4>
             <h6>{data.bgImg} </h6>
--- client/src/components/Experience.js
+++ client/src/components/Experience.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import styled from "styled-components";
 import ExpPanel from "./ExpPanel";
 import { ExpData } from "../assets/ExpData";
@@ -9,9 +11,13 @@ import ccBg from "../assets/CodeClan.png"
 import PageScroll from "./PageScroll";
 
 const ExperienceContainer = styled.section`
-    position: relative;
-    background-color: #4152de;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(2, 1fr);
+    background-color: rgb(13, 20, 34);  
     height: 100vh;
+    width: 100%;
+    overflow: hidden;
 `
 
 const ScrollContainer = styled.div`
@@ -19,18 +25,37 @@ const ScrollContainer = styled.div`
     position:absolute;
     bottom:8rem;
     left:50%;
-    transform: translateX(-50%)
+    transform: translateX(-50%);
 `
 
 
 const Experience = () => {
+    const [activeComponent, setActiveComponent] = useState(null);
+
+
     return (<ExperienceContainer id="experience" >
-        <ExpPanel data={ExpData.CC} bg={ccBg} />
-        <ExpPanel data={ExpData.RM} bg={rmBg} />
-        <ExpPanel data={ExpData.ECO} bg={ecoBg} />
+        <ExpPanel
+            data={ExpData.CC}
+            bg={ccBg}
+            active={activeComponent === 'ExPanel1'}
+            onClick={() => setActiveComponent('ExPanel1')}
+            isPrimary />
+
+        <ExpPanel
+            data={ExpData.RM}
+            bg={rmBg}
+            active={activeComponent === 'ExPanel2'}
+            onClick={() => setActiveComponent('ExPanel2')} />
+        <ExpPanel
+            data={ExpData.ECO}
+            bg={ecoBg}
+            active={activeComponent === 'ExPanel3'}
+            onClick={() => setActiveComponent('ExPanel3')} />
+
         <ScrollContainer>
             <PageScroll to="contact" />
         </ScrollContainer>
+
     </ExperienceContainer>);
 }
 
