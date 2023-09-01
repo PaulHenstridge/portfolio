c2fe2d6431dc8c02e4abd9318e0c354ba7a07764
Paul Henstridge <paulhenstridge@gmail.com>
2023-09-01 11:11:37 +0100

    Configure GitHub Pages

--- client/src/components/StaticContact.js
+++ client/src/components/StaticContact.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+
 const EmailLink = styled.h2`
     padding-top:2rem;
     color:rgb(221, 248, 251);
@@ -8,9 +9,11 @@ const EmailLink = styled.h2`
         color:red;
     }
 `
+
 const ContactIcon = styled.i`
     display:none;
 `
+
 const StaticContact = () => {
     return (  <>
     <EmailLink>I'd love to hear from you! <br></br> Please <a href="mailto:paulhenstridge@gmail.com"> drop me an email,</a> or use the links below</EmailLink>
