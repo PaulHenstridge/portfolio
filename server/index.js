25448745967e47661d9f331a990471ec7b939917
Paul Henstridge <paulhenstridge@gmail.com>
2023-09-06 10:17:31 +0100

    some progress with OAuth...

--- server/index.js
+++ server/index.js
@@ -1,76 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-// const nodemailer = require('nodemailer');
-// const { google } = require('googleapis');
-// require('dotenv').config();
-
-// app.use(cors());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// const PORT = process.env.PORT || 3001
-
-// const oauth2Client = new google.auth.OAuth2(
-//     process.env.OAUTH_CLIENT_ID,
-//     process.env.OAUTH_CLIENT_SECRET,
-//     'https://developers.google.com/oauthplayground' // This is the redirect URL where Google will send the authorization code
-// );
-
-// oauth2Client.setCredentials({
-//     refresh_token: process.env.OAUTH_REFRESH_TOKEN
-// })
-
-
-// let accessToken;
-// async function retrieveAccessToken(){
-//     try{
-//         accessToken = oauth2Client.getAccessToken();
-//     } catch (error) {
-//         console.error("error retrieving access token")
-//     }
-// }
-// retrieveAccessToken()
- 
-// let transporter = nodemailer.createTransport({
-//     service: 'gmail',
-//     auth: {
-//         type: 'OAuth2',
-//         user: process.env.EMAIL,
-//         clientId: process.env.OAUTH_CLIENT_ID,
-//         clientSecret: process.env.OAUTH_CLIENT_SECRET,
-//         refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-//         accessToken: accessToken
-//     }
-// });
-
-// const sendMail = async({ name, senderEmail, subject, message }) => {
-//     let mailOptions = {
-//         from: `${name} <${process.env.EMAIL}>`,
-//         replyTo: senderEmail,
-//         to: process.env.EMAIL,
-//         subject,
-//         text: `Message from ${name} (${senderEmail}): \n\n ${message}`
-//     };
-//     try{
-//         await transporter.sendMail(mailOptions);
-//         console.log('Email sent!');
-//         return true;
-//     } catch (error) {
-//         console.log('Error sending email:', error);
-//         return false;
-//     }
-// }
-
-// app.post('/send', async (req, res) => {
-//     const isMailSent = await sendMail(req.body);
-//     res.status(isMailSent ? 200 : 500).json({ success: isMailSent });
-// });
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
 
 require('dotenv').config();
 const express = require('express');
@@ -95,24 +22,28 @@ const oauth2Client = new google.auth.OAuth2(
     'https://developers.google.com/oauthplayground'
 );
 
-oauth2Client.setCredentials({ refresh_token: process.env.OAUTH_REFRESH_TOKEN });
 
 async function retrieveAccessToken() {
+    oauth2Client.setCredentials({ refresh_token: process.env.OAUTH_REFRESH_TOKEN });
     try {
-        const { token } = await oauth2Client.refreshAccessToken();
-        return token;
+        const { credentials } = await oauth2Client.refreshAccessToken();
+        console.log("Received credentials:", credentials);  // For debugging
+        return credentials ? credentials.access_token : null;
     } catch (error) {
         console.error("Error retrieving access token: ", error);
         if (error && error.response && error.response.data) {
             console.error("Additional error details: ", error.response.data);
         }
+        console.error("Complete error object:", JSON.stringify(error, null, 2))
         return null;
     }
 }
 
 
 app.post('/send', async (req, res) => {
+    console.log('received request ', req.body)
     const accessToken = await retrieveAccessToken();
+    console.log('access token ', accessToken)
     
     if (!accessToken) {
         return res.status(500).json({ success: false, message: 'Could not retrieve access token' });
