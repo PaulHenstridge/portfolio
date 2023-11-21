50472a11413437154f01548401eaef3cb346685f
Paul Henstridge <paulhenstridge@gmail.com>
2023-11-21 12:18:59 +0000

    added Q-API content

--- client/src/components/projectPages/QuincyInfo.js
+++ client/src/components/projectPages/QuincyInfo.js
@@ -68,6 +68,7 @@ const Bg = styled.div`
     & h5 {
       color:#5a01a7;
       font-weight:bold;
+      text-shadow:0.3px 0.3px #81777777;
     }
 
 `
@@ -141,24 +142,61 @@ const QuincyInfo = () => {
       <UpperContainer>
         <div> 
             <HeaderP>   
-            Quincy Api ........
+            An API of awesome software development learning resources published by the FreeCodeCamp commmuity.
+               This app takes all of that amazing information and makes it fully searchable,
+                with AI generated tags allowing for advanced searching within articles.
              </HeaderP>
-             
         </div>
       <DemoVid/> 
       </UpperContainer>
 
       <LowerContainer>  
         < InfoSection >
-        <p><h5>Features: </h5>
+        <p>
+        As a subscriber to the weekly newsletter of articles and tutorials from Quincy Larson of FreeCodeCamp,
+              I was excited to discover there was a json archive of all the urls shared over the years,
+               as this could be made into a great API.  <br></br><br></br>
+
+        <h4> The objectives of this project:</h4>
+        <ul>
+            <li>
+                to explore python’s module system, and its ecosystem of packages, 
+                by building a functional, useful API.
+            </li>
+            <li>
+                to write clean, DRY code that adheres to industry best practice - scalable and maintainable.
+            </li>
+            <li>
+                to push my comfort zone and develop greater fluency in python.
+             </li>
+        </ul>      
+
+
+            <h5>Features: </h5>
  All the good shizzles
                </p>
 
           <h3>Technical Considerations</h3>
 
-          <p>This project is ....<br></br><br></br>
+          <p>
+This application is built with Python, uses AI to create searchable content tags for every article,
+ stores them in a MongoDB database, and makes the articles available through a suite of RESTful endpoints.
+   A simple front end allows users to query and explore the data from the browser.<br></br><br></br>
 
-       
+   This app was built using the Python module system, in the application factory pattern.
+     In this pattern, a runner file that is outside the main project directory instantiates
+      and runs an instance of the app.  This offers great flexibility to initialise the app
+       with different configurations and data, making it highly scalable.<br></br><br></br>
+
+It connects to a MongoDB non-relational database using the pymongo library to manage interactions,
+ and mongoengine, an Object-Document Mapper, to create database schemas and interact with
+  them easily using Python.<br></br><br></br>
+
+The front end is a simple HTML page with CSS and JavaScript, which allows users
+ the opportunity to query the API.  This simple front end configuration is more
+  than enough to fulfil these requirements, and allow the entire application to
+   be deployed as one unified project.
+
           </p>
 
 
@@ -168,22 +206,60 @@ const QuincyInfo = () => {
           <h3>Professional Development</h3>
 
           <p><h4>How has this project helped me grow as a developer?</h4>  
-          <h5>...</h5>
+          <h5>python problem solving</h5>
 
-         ....
+          
+This project threw up lots if different challenges, and finding ways to solve them using
+ Python really helped develop my skills.  These included handling inconsistent fields
+  in the data [[show flexible_strptime method in get_data]], 
+   controlling the flow of data to an external API using a rate limiter
+    [[ show rate limiter in add_content_tags ]]  
+    and sanitising the AI response data before adding it to the database.[[ show sanitising code]].
      </p>
              
-         <p> <h5>8888</h5>mittens.</p>
+         <p> <h5>Style Guide</h5>
+         
+The style guide published by FreeCodeCamp made choosing colours and fonts really easy,
+ but what that ought me was the value of a well thought out style guide,
+  that allows future developers to maintain the look and feel of the application.
+    I was less than happy with the CSS in my previous python project. 
+     This style guide has given me a great tool to learn from and produce more
+      thought out and extendable css styles.</p>
            
-           <p><h5> %%</h5>
-          parsnips
+           <p><h5> Accidental Learning</h5>
+          There are somany fascinating articles in this collection I learned something every time I tested an endpoint!
 </p>
 
           <p> <h4>What were the hardest problems to overcome?</h4>
-         clingons</p>
+          The most troublesome issue resulted from one of the routes not returning the expected data. 
+          I was eventually able to pinpoint the issue to within the mongoengine library,
+           but this gave me an important decision to make:  take a deep dive into the internal workings
+            of the library, or find an alternative solution and maintain the momentum of the project.
+            <br></br><br></br>
+Considering agile principles, which advocate for adaptability and delivering working software on time,
+ I explored a workaround, using python to manipulate the data as required. 
+  [[ show python workaround line 150 in link_controller ]]
+  <br></br><br></br>
+A more uniform approach using mongoengine remains an objective,
+ but by applying agile thinking I was able to move forward and deliver a working project,
+  ask well as develop my problem solving skills.
+</p>
 
           <p> <h4>What would I do differently if starting again today?</h4>
-            ........
+          If restarting this project today, with the knowledge I now have, I would look at using a more
+           powerful AI model to provide content tags.  The decision was largely driven by cost.
+            Processing all the articles with GPT3.5 in September 2023 cost $9, and to use GPT4 would 
+            have cost $180.  <br></br>As of November 2023, token prices for GPT4 have fallen considerably,
+             pricing the same operation closer to $60, and this trend will likely continue.
+               A more intuitive model would improve the quality of tagging and reduce the need
+                for sanitisation.<br></br><br></br>
+
+To increase my exposure to the python ecosystem, I might consider exploring 
+the Django framework as an alternative to Flask. 
+Although this project is fairly simple in its current state, there are many exciting opportunities to
+ extend it. As the project grows, Django's comprehensive set of features could offer more built-in tools
+  and functionalities that are ideal for larger-scale applications.
+
 
         </p>
         </InfoSection>
@@ -192,27 +268,21 @@ const QuincyInfo = () => {
           <h3>Next Steps</h3>
 
           
-          <p><h5>ideas</h5>  
-      wibble
+          <p><h5>Expanded front end application</h5>  
+      
 
+The front end could be developed as a means of interacting with the content. 
+  Effectively a separate application that consumes the API, 
+   it could facilitate:
+   <ul>
+    <li>User profiles</li>
+    <li>Search history</li>
+    <li>Favourite articles</li>
+    <li>Accessibility preferencess</li>
+    <li>User profiles</li>
+    <li>Document reader</li>
+   </ul>
 </p>
-          <p><h5>Further Extensions </h5>  
-          <ul>
-            <li><strong> Team Selection </strong> Choose which players take part in the next game  </li>
-            <li><strong> Knock-out Competition </strong>      Introduce a Tournament class that will handle the selection of teams, and arrange knock-out matches to determine a champion
-  </li>
-            <li><strong> Training </strong> a train() method on Player could increase the players skill, and hence value.
-  </li>
-            <li><strong> Fatigue </strong> Introduce a fatigue attribute on Player, which increases after games or training,
-             potentially affects skill level.
-  </li>
-            <li><strong> Fouls/ Injuries </strong> Update the game simulation algorithm to include injuries and fouls
-  </li>
-            <li><strong> Additional attributes </strong> Attributes such as Speed, Flair, Aggression could be included on Player, and feed into Team properties.
-  </li>
-          </ul>
- </p>
-
         </InfoSection>
       
       </LowerContainer>
